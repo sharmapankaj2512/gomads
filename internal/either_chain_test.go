3bcbcb341ebfd1d03c86d91238249d5afcd60740
Pankaj Sharma <pankaj.sharma@talabat.com>
2023-01-01 03:50:50 +0400

    chaining

--- internal/either_chain_test.go
+++ internal/either_chain_test.go
@@ -7,8 +7,14 @@ import (
 
 func TestName(t *testing.T) {
 	left := NewLeft[int, error](1)
-	newLeft := MapLeft(left, func(v int) int { return v + 1 })
-	ResolveLeftOne(newLeft, func(v int) {
-		assert.Equal(t, 2, v)
-	})
+	Pipe(MapLeft(left, func(v int) int { return v + 1 }),
+		func(newLeft Either[int, error]) {
+			ResolveLeftOne(newLeft, func(v int) {
+				assert.Equal(t, 2, v)
+			})
+		})
+}
+
+func Pipe[L any, R any](left Either[L, R], f func(newLeft Either[L, R])) {
+	f(left)
 }
