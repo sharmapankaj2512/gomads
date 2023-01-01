812ed91ac477e39119cfbd8c84900137c9e02dbf
Pankaj Sharma <pankaj.sharma@talabat.com>
2023-01-01 12:10:45 +0400

    refactor format code

--- internal/either_chain_test.go
+++ internal/either_chain_test.go
@@ -6,11 +6,8 @@ import (
 )
 
 func TestResolveLeftOne(t *testing.T) {
-	left := NewLeft[int, error](1)
-	addOne := func(v int) int { return v + 1 }
-	assertIsTwo := func(v1 int) { assert.Equal(t, 2, v1) }
-
-	Pipe(
-		MapLeft(left, addOne),
-		ResolveLeftOne[int, error](assertIsTwo))
+	Pipe(MapLeft(NewLeft[int, error](1), func(v int) int { return v + 1 }),
+		ResolveLeftOne[int, error](func(v1 int) {
+			assert.Equal(t, 2, v1)
+		}))
 }
