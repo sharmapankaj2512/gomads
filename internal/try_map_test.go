dba309e058cd842a91d330486775188d69c90502
Pankaj Sharma <pankaj.sharma@talabat.com>
2023-01-03 08:34:39 +0400

    extract success and failure types

--- internal/try_map_test.go
+++ internal/try_map_test.go
@@ -10,33 +10,41 @@ func TestTransformSuccess(t *testing.T) {
 	success := SuccessFor(1)
 	transformed := Map(success, func(v int) int { return v + 1 })
 
-	assert.Equal(t, 2, transformed.success)
+	assert.Equal(t, 2, transformed.success.value)
 }
 
 func TestTransformError(t *testing.T) {
 	err := ErrorAs[int]("something went wrong")
 	transformed := MapError(err, func(v error) error { return errors.New("failed") })
 
-	assert.Equal(t, "failed", transformed.err.Error())
+	assert.Equal(t, "failed", transformed.err.value.Error())
 }
 
-func MapError[S any](err Try[S], f func(v error) error) Try[S] {
-	return ErrorAs[S](f(err.err).Error())
+type Success[S any] struct {
+	value S
+}
+
+type Failure struct {
+	value error
 }
 
 type Try[S any] struct {
-	success S
-	err     error
+	success Success[S]
+	err     Failure
 }
 
 func SuccessFor[S any](value S) Try[S] {
-	return Try[S]{success: value}
-}
-
-func Map[S any, NS any](try Try[S], f func(v S) NS) Try[NS] {
-	return SuccessFor(f(try.success))
+	return Try[S]{success: Success[S]{value: value}}
 }
 
 func ErrorAs[S any](message string) Try[S] {
-	return Try[S]{err: errors.New(message)}
+	return Try[S]{err: Failure{value: errors.New(message)}}
+}
+
+func Map[S any, NS any](try Try[S], f func(v S) NS) Try[NS] {
+	return SuccessFor(f(try.success.value))
+}
+
+func MapError[S any](err Try[S], f func(v error) error) Try[S] {
+	return ErrorAs[S](f(err.err.value).Error())
 }
