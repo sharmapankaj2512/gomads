ad68fc33589f6f019b168ec90fb3de69267249d5
Pankaj Sharma <pankaj.sharma@talabat.com>
2023-01-03 08:36:35 +0400

    move try to a separate file

--- internal/try_map_test.go
+++ internal/try_map_test.go
@@ -19,32 +19,3 @@ func TestTransformError(t *testing.T) {
 
 	assert.Equal(t, "failed", transformed.err.value.Error())
 }
-
-type Success[S any] struct {
-	value S
-}
-
-type Failure struct {
-	value error
-}
-
-type Try[S any] struct {
-	success Success[S]
-	err     Failure
-}
-
-func SuccessFor[S any](value S) Try[S] {
-	return Try[S]{success: Success[S]{value: value}}
-}
-
-func ErrorAs[S any](message string) Try[S] {
-	return Try[S]{err: Failure{value: errors.New(message)}}
-}
-
-func Map[S any, NS any](try Try[S], f func(v S) NS) Try[NS] {
-	return SuccessFor(f(try.success.value))
-}
-
-func MapError[S any](err Try[S], f func(v error) error) Try[S] {
-	return ErrorAs[S](f(err.err.value).Error())
-}
